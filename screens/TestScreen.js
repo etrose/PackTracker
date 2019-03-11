d0198f7bddaa9fcd36bfb79fcff4afbc6e343b50
etrose <evan.rose.15@cnu.edu>
2019-03-11 01:48:31 -0400

    Search Function Testing

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -3,7 +3,8 @@ import {
   StyleSheet,
   View,
   FlatList,
-  Button,
+  Text,
+  TouchableOpacity,
 } from 'react-native';
 
 import Colors from '../constants/Colors';
@@ -19,18 +20,40 @@ class TestScreen extends React.Component {
     super(props);
     this.state = {
       searchInput: '',
+      snapShot: '',
     };
+    this.ref = firebase.firestore().collection("users");
   }
   static navigationOptions = {
     header: null,
   };
+
+  componentDidMount() {
+    // this.unsubscribe = this.ref.onSnapshot((querySnapshot) => {
+    //   this.setState({
+    //     snapShot: querySnapshot,
+    //   });
+    //   console.log(this.state.snapShot);
+    // });
+  }
+
+  doSearch = () => {
+    alert("Searching" + this.state.searchInput);
+    firebase.firestore().collection("users").where("username", "==", this.state.searchInput).get().then((results) => {
+      results.forEach((doc) => {
+        console.log(doc.data());
+        alert(doc.data().email);
+      });
+    });
+    
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.searchContainer}>
         <SearchBar
-          containerStyle={{ flex: 1, backgroundColor: Colors.tintColor, alignItems: 'center'}}
+          containerStyle={{ flex: 1, backgroundColor: Colors.background, alignItems: 'center'}}
           inputContainerStyle={{ backgroundColor: Colors.background, margin: 0,}}
           onChangeText={text =>this.setState({searchInput:text})}
           value={this.state.searchInput}
@@ -38,8 +61,11 @@ class TestScreen extends React.Component {
           lightTheme
           round
         />
-        <Button style={styles.searchButton} title="GO" onPress={()=>alert(this.state.searchInput)}/>
+        {/* <TouchableOpacity style={styles.searchButton} title="GO" onPress={()=>alert(this.state.searchInput)}>
+        <Text>Go</Text>
+        </TouchableOpacity> */}
         </View>
+        <TouchableOpacity onPress={this.doSearch}><Text>Search!</Text></TouchableOpacity>
       </View>
     );
   }
@@ -56,10 +82,15 @@ const styles = StyleSheet.create({
   searchContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.background,
     minWidth: "100%",
     maxWidth: "100%",
   },
   searchButton: {
-    width: "10%",
+    justifyContent: 'center',
+    width: 30,
+    height: 30,
+    backgroundColor: Colors.colorSecondary,
   },
 });
